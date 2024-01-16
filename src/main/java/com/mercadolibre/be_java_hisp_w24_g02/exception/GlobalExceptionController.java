598285ffa3ff88d6e6af4e2b25d050e7b0973a61
Jose Joaquin Cabello Alegria <joscabello@cl000xxqcdxm5qf.ml.com>
2024-01-16 13:02:05 -0300

    [fix] US0005 delete unused import in GlobalExceptionController

--- src/main/java/com/mercadolibre/be_java_hisp_w24_g02/exception/GlobalExceptionController.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w24_g02/exception/GlobalExceptionController.java
@@ -7,11 +7,10 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@ControllerAdvice()
+@ControllerAdvice
 public class GlobalExceptionController {
 
     @ExceptionHandler(NotFoundException.class)

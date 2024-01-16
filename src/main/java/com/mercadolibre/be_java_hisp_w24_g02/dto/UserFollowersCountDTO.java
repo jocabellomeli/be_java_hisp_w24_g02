142ec862e76fd21636a8ef80a8f9c060e05bd345
jeguevarap <juan.guevara@mercadolibre.com.co>
2024-01-16 15:21:34 -0500

    [FEAT] Fix merge conflicts

--- src/main/java/com/mercadolibre/be_java_hisp_w24_g02/dto/UserFollowersCountDTO.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w24_g02/dto/UserFollowersCountDTO.java
@@ -1,7 +1,15 @@
 package com.mercadolibre.be_java_hisp_w24_g02.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
 public record UserFollowersCountDTO(
-        Integer user_id,String user_name, Integer followers_count)
+        @JsonProperty("user_id")
+        Integer userId,
+        @JsonProperty("user_name")
+        String userName,
+        @JsonProperty("followers_count")
+        Integer followersCount)
 {
 
 }

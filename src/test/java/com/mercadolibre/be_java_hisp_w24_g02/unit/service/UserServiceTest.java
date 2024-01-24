0d483e9c0fa84fc470af807a3a9accef972a8dec
stagliaferri <stefanoguillermo.tagliaferri@mercadolibre.com>
2024-01-24 16:57:35 -0300

    test: testing getFollowers and getFollowed method with sorting

--- src/test/java/com/mercadolibre/be_java_hisp_w24_g02/unit/service/UserServiceTest.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w24_g02/unit/service/UserServiceTest.java
@@ -1,11 +1,23 @@
 package com.mercadolibre.be_java_hisp_w24_g02.unit.service;
 
+import com.mercadolibre.be_java_hisp_w24_g02.dto.UpdateToRelationshipsDTO;
+import com.mercadolibre.be_java_hisp_w24_g02.dto.UserBasicInfoDTO;
+import com.mercadolibre.be_java_hisp_w24_g02.dto.UserRelationshipsDTO;
+import com.mercadolibre.be_java_hisp_w24_g02.entity.User;
 import com.mercadolibre.be_java_hisp_w24_g02.repository.interfaces.IUserRepository;
 import com.mercadolibre.be_java_hisp_w24_g02.service.implementations.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -15,6 +27,246 @@ public class UserServiceTest {
 
     @Mock
     private IUserRepository userRepository;
+
+    @Test
+    @DisplayName("Test for checking if the followed user list is sorted correctly ascendent")
+    public void getFollowedUsersSortedNameAscTest(){
+        // arrange
+
+        List<User> followed = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+
+        List<User> followers = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+        User user1 = new User(1, "Usuario 1", followers, followed, null, null);
+
+
+        List<UserBasicInfoDTO> sortedFollowed = new ArrayList<>(List.of(
+                new UserBasicInfoDTO(2, "Usuario 2"),
+                new UserBasicInfoDTO(3, "Usuario 3"),
+                new UserBasicInfoDTO(4, "Usuario 4")
+        ));
+
+        UserRelationshipsDTO expected = new
+                UserRelationshipsDTO(1, "Usuario 1", sortedFollowed, false);
+
+        String order = "name_asc";
+
+        // act
+
+        Mockito.when(userRepository.findById(1)).thenReturn(Optional.of(user1));
+
+        UserRelationshipsDTO result = service.getUserFollowed(1, order);
+
+        // assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Test for checking if the followed user list is sorted correctly descendent")
+    public void getFollowedUsersSortedNameDescTest(){
+        // arrange
+
+        List<User> followed = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+
+        List<User> followers = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+        User user1 = new User(1, "Usuario 1", followers, followed, null, null);
+
+
+        List<UserBasicInfoDTO> sortedFollowed = new ArrayList<>(List.of(
+                new UserBasicInfoDTO(4, "Usuario 4"),
+                new UserBasicInfoDTO(3, "Usuario 3"),
+                new UserBasicInfoDTO(2, "Usuario 2")
+        ));
+
+        UserRelationshipsDTO expected = new
+                UserRelationshipsDTO(1, "Usuario 1", sortedFollowed, false);
+
+        String order = "name_desc";
+
+        // act
+
+        Mockito.when(userRepository.findById(1)).thenReturn(Optional.of(user1));
+
+        UserRelationshipsDTO result = service.getUserFollowed(1, order);
+
+        // assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Test for checking if the followers user list is sorted correctly ascendent")
+    public void getFollowersUsersSortedNameAscTest(){
+        // arrange
+
+        List<User> followed = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+
+        List<User> followers = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+        User user1 = new User(1, "Usuario 1", followers, followed, null, null);
+
+
+        List<UserBasicInfoDTO> sortedFollowers = new ArrayList<>(List.of(
+                new UserBasicInfoDTO(2, "Usuario 2"),
+                new UserBasicInfoDTO(3, "Usuario 3"),
+                new UserBasicInfoDTO(4, "Usuario 4")
+        ));
+
+        UserRelationshipsDTO expected = new
+                UserRelationshipsDTO(1, "Usuario 1", sortedFollowers, true);
+
+        String order = "name_asc";
+
+        // act
+
+        Mockito.when(userRepository.findById(1)).thenReturn(Optional.of(user1));
+
+        UserRelationshipsDTO result = service.getUserFollowers(1, order);
+
+        // assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Test for checking if the followers user list is sorted correctly descendent")
+    public void getFollowersUsersSortedNameDescTest(){
+        // arrange
+
+        List<User> followed = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+
+        List<User> followers = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+        User user1 = new User(1, "Usuario 1", followers, followed, null, null);
+
+
+        List<UserBasicInfoDTO> sortedFollowers = new ArrayList<>(List.of(
+                new UserBasicInfoDTO(4, "Usuario 4"),
+                new UserBasicInfoDTO(3, "Usuario 3"),
+                new UserBasicInfoDTO(2, "Usuario 2")
+        ));
+
+        UserRelationshipsDTO expected = new
+                UserRelationshipsDTO(1, "Usuario 1", sortedFollowers, true);
+
+        String order = "name_desc";
+
+        // act
+
+        Mockito.when(userRepository.findById(1)).thenReturn(Optional.of(user1));
+
+        UserRelationshipsDTO result = service.getUserFollowers(1, order);
+
+        // assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Test for checking if the followed user list is not sorted when order is none")
+    public void getFollowedUsersSortedNoneTest(){
+        // arrange
+
+        List<User> followed = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+
+        List<User> followers = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+        User user1 = new User(1, "Usuario 1", followers, followed, null, null);
+
+
+        List<UserBasicInfoDTO> sortedFollowed = new ArrayList<>(List.of(
+                new UserBasicInfoDTO(2, "Usuario 2"),
+                new UserBasicInfoDTO(4, "Usuario 4"),
+                new UserBasicInfoDTO(3, "Usuario 3")
+        ));
+
+        UserRelationshipsDTO expected = new
+                UserRelationshipsDTO(1, "Usuario 1", sortedFollowed, false);
+
+        String order = "none";
+
+        // act
+
+        Mockito.when(userRepository.findById(1)).thenReturn(Optional.of(user1));
+
+        UserRelationshipsDTO result = service.getUserFollowed(1, order);
+
+        // assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Test for checking if the followers user list is not sorted when order is none")
+    public void getFollowersUsersSortedNoneTest(){
+        // arrange
+
+        List<User> followed = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+
+        List<User> followers = new ArrayList<>(List.of(
+                new User(2, "Usuario 2", null, null, null, null),
+                new User(4, "Usuario 4", null, null, null, null),
+                new User(3, "Usuario 3", null, null, null, null)
+        ));
+        User user1 = new User(1, "Usuario 1", followers, followed, null, null);
+
+
+        List<UserBasicInfoDTO> sortedFollowers = new ArrayList<>(List.of(
+                new UserBasicInfoDTO(2, "Usuario 2"),
+                new UserBasicInfoDTO(4, "Usuario 4"),
+                new UserBasicInfoDTO(3, "Usuario 3")
+        ));
+
+        UserRelationshipsDTO expected = new
+                UserRelationshipsDTO(1, "Usuario 1", sortedFollowers, true);
+
+        String order = "none";
+
+        // act
+
+        Mockito.when(userRepository.findById(1)).thenReturn(Optional.of(user1));
+
+        UserRelationshipsDTO result = service.getUserFollowers(1, order);
+
+        // assert
+        Assertions.assertEquals(expected, result);
+    }
 
 
 
